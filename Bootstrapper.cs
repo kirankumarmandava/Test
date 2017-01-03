bbc46d33dfded3c2d7c81f0fc69f7206131d818d
kirankumarmandava <kiran1344@gmail.com>
2017-01-03 13:38:29 +0530

    Add files via upload

---
+++ Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using System.Web.Mvc;
+using MyFirstApp.Data.Infra;
+using MyFirstApp.Data.Repository;
+using MyFirstApp.BLL;
+
+namespace MyFirstApp
+{
+    public class Bootstrapper
+    {
+        public static void SetAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+            builder.RegisterType<DatabaseFactory>().As<IDatabaseFactory>().InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .Where(t => t.Name.EndsWith("Repository"))
+            .AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            //builder.RegisterType<MenuService>().As<IMenuService>().InstancePerHttpRequest();
+            builder.RegisterAssemblyTypes(typeof(MenuService).Assembly)
+            .Where(t => t.Name.EndsWith("Service"))
+            .AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterFilterProvider();
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ FirstAppContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MyFirstApp.Models;
+using MyFirstApp.Data.Model;
+
+namespace MyFirstApp
+{
+    public class FirstAppContext : DbContext
+    {
+        public DbSet<UserMaster> users { get; set; }
+        public DbSet<MenuMaster> menus { get; set; }
+        public DbSet<RoleMaster> roles { get; set; }
+
+        public virtual void Commit()
+        {
+            base.SaveChanges();
+        }
+ 
+    }
+}
